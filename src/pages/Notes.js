6fd7cc82b9e19805960104486a95e7c19d625033
Kyle Lynch <kylelynch104@gmail.com>
2021-03-19 14:57:14 -0500

    create notes form

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,8 +1,18 @@
-
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
 
 const Notes = () => {
   return (
-  <div>Notes page</div>
+    <Container>
+      <Typography
+        variant='h5'
+        component='h1'
+        gutterBottom
+        color='textSecondary'
+      >
+        Notes page
+      </Typography>
+    </Container>
   )
 }
 
