453abd44d914bf5b1096131bafac6161be658350
Kyle Lynch <kylelynch104@gmail.com>
2021-03-29 12:55:12 -0500

    menu routes with history, return functions with makeStyles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,35 +1,85 @@
 import { makeStyles } from '@material-ui/core'
 import Drawer from '@material-ui/core/Drawer'
 import Typography from '@material-ui/core/Typography'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons'
+import { useHistory, useLocation } from 'react-router'
 
 const drawerWidth = 240
 
-const useStyles = makeStyles({
-  root: {
-    display: 'flex'
-  },
-  page: {
-    width: '100%',
-    background: '#f9f9f9',
-  },
-  drawer: {
-    width: drawerWidth
-  },
-  drawerPaper: {
-    width: drawerWidth
-  },
+const useStyles = makeStyles(theme => {
+  return {
+    root: {
+      display: 'flex',
+    },
+    page: {
+      width: '100%',
+      background: '#f9f9f9',
+      padding: theme.spacing(3),
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    active: {
+      background: '#f4f4f4',
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+  }
 })
 
 const Layout = ({ children }) => {
   const classes = useStyles()
+  const history = useHistory()
+  const location = useLocation()
+
+  const menuItems = [
+    {
+      text: 'My Notes',
+      icon: <SubjectOutlined color='secondary' />,
+      path: '/',
+    },
+    {
+      text: 'Create Note',
+      icon: <AddCircleOutlineOutlined color='secondary' />,
+      path: '/create',
+    },
+  ]
+
   return (
     <div className={classes.root}>
       {/* App Bar */}
 
-      <Drawer className={classes.drawer} variant='permanent' anchor='left' classes={{ paper: classes.drawerPaper }}>
+      <Drawer
+        className={classes.drawer}
+        variant='permanent'
+        anchor='left'
+        classes={{ paper: classes.drawerPaper }}
+      >
         <div>
-          <Typography variant='h5'>Fresh Notes</Typography>
+          <Typography variant='h5' className={classes.title}>Fresh Notes</Typography>
         </div>
+
+        <List>
+          {menuItems.map(item => (
+            <ListItem
+              key={item.text}
+              button
+              onClick={() => history.push(item.path)}
+              className={location.pathname === item.path && classes.active}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text}></ListItemText>
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
 
       <div className={classes.page}>{children}</div>
--- src/components/NotesCard.js
+++ src/components/NotesCard.js
@@ -4,25 +4,38 @@ import CardContent from '@material-ui/core/CardContent'
 import IconButton from '@material-ui/core/IconButton'
 import DeleteOutlined from '@material-ui/icons/DeleteOutlined'
 import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  test: {
+    border: note => {
+      if (note.category === 'work') {
+        return '1px solid red'
+      }
+    },
+  },
+})
 
 const NotesCard = ({
-  note: { title, details, category, id },
+  note,
   handleDelete,
 }) => {
+  const classes = useStyles(note)
+
   return (
-    <Card elevation={1}>
+    <Card elevation={1} className={classes.test}>
       <CardHeader
         action={
-          <IconButton aria-label='settings' onClick={() => handleDelete(id)}>
+          <IconButton aria-label='settings' onClick={() => handleDelete(note.id)}>
             <DeleteOutlined />
           </IconButton>
         }
-        title={title}
-        subheader={category}
+        title={note.title}
+        subheader={note.category}
       />
       <CardContent>
         <Typography variant='body2' color='textSecondary'>
-          {details}
+          {note.details}
         </Typography>
       </CardContent>
     </Card>
